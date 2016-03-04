f5acf08315f7ae0e5ab4d7578ea5bdb3978a312a
Charlie Allen <miss.neophyte@gmail.com>
2016-03-04 08:32:55 +0000

    add method added for cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,5 +6,10 @@ class CartsController < ApplicationController
   	@cart_movies = Movie.find(cart_ids)
   end
 
+  def add
+  	$redis.sadd current_user_cart, params[:movie_id]
+  	render json: current_user_cart.cart_count, status: 200
+  end
+
   
 end
