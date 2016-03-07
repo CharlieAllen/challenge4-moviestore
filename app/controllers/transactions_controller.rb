09188c86466cd471b7b1ee51c28944529e74a29a
Charlie Allen <miss.neophyte@gmail.com>
2016-03-07 15:44:05 +0000

    routes.rb and transactions controller updated

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,6 @@
 class TransactionsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :check_cart!
   def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
 	devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 	resources :movies, only: [:show, :index]
+	resources :transactions, only: [:new, :create]
 	resource :cart, only: [:show] do
 	  put 'add/:movie_id', to: 'carts#add', as: :add_to
 	  put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
