e41b1bb281226f1bfbc2bd95063578edf75c57cd
Charlie Allen <miss.neophyte@gmail.com>
2016-03-07 12:27:43 +0000

    migration rolled back to include additional information and then re-run

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20160304102413) do
     t.datetime "updated_at", null: false
   end
 
+  add_index "purchases", ["movie_id", "buyer_id"], name: "index_purchases_on_movie_id_and_buyer_id", unique: true, using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
