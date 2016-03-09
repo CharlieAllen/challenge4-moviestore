5238d85c625f0b4f6d39805bf0589cecb12649e2
Charlie Allen <miss.neophyte@gmail.com>
2016-03-09 15:46:05 +0000

    db now seeding

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,19 +1,19 @@
 class Movie < ActiveRecord::Base
   has_many :purchases
   has_many :buyers, through: :purchases
-
-  before_save :embed_video_url
+  validates_presence_of :title, :release_year, :price, :description, :imdb_id, :poster_url, :video_url
 
   def poster
-    "http://ia.media-imdb.com/images/M/#{poster_url}"
+    return "http://ia.media-imdb.com/images/M/#{poster_url}"
   end
 
   def imdb
-    "http://www.imdb.com/title/#{imdb_id}/"
+    return "http://www.imdb.com/title/#{imdb_id}/"
   end
 
   def embed_video_url
-    self.video_url = "//www.youtube.com/embed/#{video_url.split('v=')[1].split('&list')[0]}"
+    return nil unless video_url.respond_to?(:split)
+    return "//www.youtube.com/embed/#{video_url.split('v=')[1].split('&list')[0]}"
   end
 
   def cart_action(current_user_id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'csv'
-CSV.foreach(Rails.root.join("db/seeds_data/movies.csv"), headers: true) do |row|
-  Movie.find_or_create_by(title: row[0], release_year: row[1], price: row[2], description: row[3], imdb_id: row[4], poster_url: row[5])
+
+feedback = []
+counter = 0
+
+CSV.foreach(Rails.root.join("db/seeds_data/movies.csv"), headers: true, skip_blanks: true, encoding: 'UTF-8') do |row|
+  m = Movie.find_or_create_by(title: row[0], release_year: row[1], price: row[2], description: row[3], imdb_id: row[4], poster_url: row[5], video_url: row[6])
+  # m = Movie.find_or_initialize_by(title: row[0], release_year: row[1], price: row[2], description: row[3], imdb_id: row[4], poster_url: row[5], video_url: row[6])
+  # byebug
+  # puts m.video_url
+  # m.save
+
+  feedback << { row: counter, persisted: m.persisted?, errors: m.errors.messages, valid: m.valid?, new: m.new_record? }
+  counter += 1
 end
+
+puts feedback.to_yaml
